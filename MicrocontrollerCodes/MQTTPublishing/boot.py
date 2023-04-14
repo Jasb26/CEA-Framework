8eadd31d2a049c62b59950584dd97c9067bfe383
Jasmine Bowie <122796834+Jasb26@users.noreply.github.com>
2023-04-14 15:11:16 +0100

    Create boot.py

---
+++ MicrocontrollerCodes/MQTTPublishing/boot.py
@@ -0,0 +1,13 @@
+def connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        sta_if.active(True)
+        sta_if.connect(WIFI_NAME, WIFI_PASS)
+        while not sta_if.isconnected():
+            pass # wait till connection
+    print('network config:', sta_if.ifconfig())
+
+WIFI_NAME = 'Jas' #connected using my phone hotspot as router
+WIFI_PASS = 'jassyb123'
+connect()
